278fcc6405330578a4b594f16e91c7147eb46c13
Marvin Jay <marvin@bluejayinteractive.co.uk>
2019-02-18 12:37:32 +0000

    update

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,10 +22,6 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('form-test');
   });
 
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to form-test!');
-  });
+  
+
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,8 @@ import data from './data.json';
   styleUrls: ['./app.component.scss']
 })
 
-
-
 export class AppComponent {
 
-  
-  
-  groupList = [];
   groupItems = [];
   itemList = [];
   splitList = [];
@@ -21,22 +16,21 @@ export class AppComponent {
   totalArr = [];
   
   ngOnInit() {
-
+    // get the list of groups and items from the json
     const groups = (<any>data).groups;
     const items = (<any>data).items;
-
+    // create a formatted key : value version of the items data
     for (let item in items) {
       this.itemList.push({name: item,  amount:  items[item]});
     }
-
-   for (let group in groups) {
+    // create a master version of the data that contains group - item - value
+    for (let group in groups) {
       for (let tempItem of groups[group] ){
         var result = this.itemList.filter(obj => {
           return obj.name === tempItem
         })
         this.groupItems.push({group: group, itemName: tempItem, itemAmount: result[0].amount})
         groups[group].amount = result[0].amount;
-        // console.log(groups[group]);
       }
     }
 
@@ -48,51 +42,36 @@ export class AppComponent {
       }, {});
     }
 
-    function getSum(total, num) {
-      return total + num;
-    }
-
-
     this.splitList = groupBy(this.groupItems, 'group'); // => {stationary:[...], food:[...]}
 
     for (let listTypes in this.splitList){
-      console.log(listTypes);
       this.tempTotal = [];
       for (let values of this.splitList[listTypes] ){
-        console.log(values.itemAmount);
         this.tempTotal.push(values.itemAmount)
       }
+
       let total = this.tempTotal.reduce((a, b) => a + b, 0);
-      
       this.totalArr.push({listName: listTypes, totals: total})
-      console.log(this.totalArr); // 6
     }
-
-    // console.log(this.splitList);
-    // console.log(this.itemList);
-   
   }
 
-  
-
-
-  onBlurMethod(l, e){
-   console.log(l + ' : ' + e);
-   const inputValue = e.target.value;
-   console.log(inputValue);
-   var elms = document.getElementsByClassName(l)
-    for (var i = 0; i < elms.length; i++) {
-      if(i == 1){
-        // elms[i].value(inputValue);
+  // on blur of group textbox get the name of the group and find the elements with matching class name
+  onBlurMethod(listTitle, event){
+   console.log(listTitle + ' : ' + event);
+   const inputValue = event.target.value;
+   var elms = document.getElementsByClassName(listTitle);
+   // Validation number between 0 - 999
+    if (inputValue.length > 3){
+      alert("Amount must be between 0 - 999");
+    }else{
+      // update all the values for that group
+      for (var i = 0; i < elms.length; i++) {
+        if(i == 0){
+          elms[i].innerHTML = inputValue;
+        }else{
+          elms[i].innerHTML = '0';
+        }
       }
-      // if (elms[i].getAttribute("value") === "account"){
-      // elms[i].setValue('0')
-      // control => control.get(l).setValue('0');
-      // control[l].setValue('0');
-      // }
     }
   }
-
-
-  title = 'Developer Test';
 }
